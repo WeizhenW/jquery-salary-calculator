d85682a471c768c71ac0afbe9173f567e95bfd42
Weizhen Wang <wangwzhen@gmail.com>
2019-05-17 15:28:46 -0500

    input function

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
 $(document).ready(onReady);
+
+let employees = {};
+
 function onReady() {
-    console.log('jq ready');
+    $('#submitButton').on('click', addEmployee);
+
+}
+
+
+function addEmployee() {
+    let firstName = $("#firstName").val();
+    let lastName = $("#lastName").val();
+    let employeeId = $("#employeeId").val();
+    let title = $("#title").val();
+    let annualSalary = $("#annualSalary").val();
+
+    $('#salaryTableBody').append(`
+
+    <tr>
+        <td>${firstName}</td>
+        <td>${lastName}</td>
+        <td>${employeeId}</td>
+        <td>${title}</td>
+        <td>${annualSalary}</td>
+        <td><button class="deleteButton">Delete</button></td>
+    </tr> 
+    `)
+
+    $('input').val('');
 }
