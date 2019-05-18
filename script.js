fb52fde980d73cadc7ff00b5111b56db3a9ac8fc
Weizhen Wang <wangwzhen@gmail.com>
2019-05-17 21:40:29 -0500

    completed

--- script.js
+++ script.js
@@ -26,11 +26,12 @@ let employees = [{
 let totalMonthly = 0;
 
 function onReady() {
+    appendTable();//loop through array and add employees to table
     $('#submitButton').on('click', addEmployee);
     $('#salaryTableBody').on('click', '.deleteButton', deleteEmployee);
 }
 
-//add employee to the table and to the array
+//add employee to the array from the input value
 function addEmployee() {
     //get value from input
     let firstName = $("#firstName").val();
@@ -39,20 +40,7 @@ function addEmployee() {
     let title = $("#title").val();
     let annualSalary = $("#annualSalary").val();
 
-    //append to table
-    $('#salaryTableBody').append(`
-    <tr>
-        <td>${firstName}</td>
-        <td>${lastName}</td>
-        <td class="employeeId">${employeeId}</td>
-        <td>${title}</td>
-        <td class="annualSalary">${annualSalary}</td>
-        <td><button class="deleteButton">Delete</button></td>
-    </tr> 
-    `)
-    //clear out the input
-    $('input').val('');
-    //push to the array
+    //push values to the array
     employees.push({
         firstName: firstName,
         lastName: lastName,
@@ -60,27 +48,49 @@ function addEmployee() {
         title: title,
         annualSalary: annualSalary
     })
-    console.log(employees);
+
+    //clear out the input
+    $('input').val('');
+    
+    //call appendTable function to loop through array and append to table
+    appendTable();
+    //calculate the new monthly total
     calculateTotal();
+}
+
+function appendTable() {
+
+    //clear the existing table content
+    $('.employeeRow').remove();
+    //loop through array to add all employees to table
+    for(employee of employees) {
+        $('#tableBottom').before(`
+    <tr class="employeeRow">
+        <td>${employee.firstName}</td>
+        <td>${employee.lastName}</td>
+        <td class="employeeId">${employee.employeeId}</td>
+        <td>${employee.title}</td>
+        <td class="annualSalary">$ ${employee.annualSalary}</td>
+        <td class="deleteButtonTd"><button class="deleteButton">Delete</button></td>
+    </tr> 
+    `)
+    }
 }
 
 //delete employee from table and from the array
 function deleteEmployee() {
     //remove the employee from the array
-    // let salaryRemoved = $(this).closest('tr').children('.annualSalary').text();
-    let IdRemoved = $(this).closest('tr').children('.employeeId').text();
-
+    let IdRemoved = $(this).closest('tr').children('.employeeId').text(); //get the id of the row clicked
+    //loop through array to find the employee with this id and delete it from the array
     for(let i in employees) {
         if(employees[i].employeeId === IdRemoved) {
             employees.splice(i, 1);
         }
     }
 
-    console.log(employees);
-
-    //remove the row from the table
-    $(this).closest('tr').remove();
-
+    //call function to update the table
+    appendTable();
+    //update the monthly total
     calculateTotal();
   
 }
@@ -93,7 +103,7 @@ function calculateTotal() {
     }
     //update total in the DOM
     $('#totalMonthly').text(totalMonthly);
-    //check if exceeds 20,000
+    //check if exceeds $20,000
     if(totalMonthly > 200000) {
         $('#totalMonthly').css('background-color', 'red');
     } else {
